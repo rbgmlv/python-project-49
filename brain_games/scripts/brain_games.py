2a2e1b0a73b24cd308eb809a92ecca0f3c450816
Ruslan Bogomolov <bogomolov-rs@zvuk.com>
2024-12-20 19:09:52 +0300

    Added cli

--- brain_games/scripts/brain_games.py
+++ brain_games/scripts/brain_games.py
@@ -1,8 +1,10 @@
-from brain_games.module import greet
+from brain_games.cli import welcome_user
+from brain_games.greeting import greet
 
 
 def main():
     greet()
+    welcome_user()
 
 
 if __name__ == "__main__":
